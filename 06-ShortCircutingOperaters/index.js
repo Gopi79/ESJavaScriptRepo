da95fe3155e216d47a05e7e68e999d353e794daa
gopikrishna <gopikrishnaboppudi786@gmail.com>
2024-01-05 21:37:17 +0530

    ShortCircutingOperaters commited

---
+++ 06-ShortCircutingOperaters/index.js
@@ -0,0 +1,15 @@
+//Shot circuting Operaters
+
+//&&
+console.log( true && "good" );
+
+console.log( false && "bad" );
+
+// ||
+
+console.log( true || "good" );
+
+console.log( false || "bad" );
+
+// ??
+
